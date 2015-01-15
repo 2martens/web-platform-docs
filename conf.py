6769cefec5b612609518b53967748516737a5b29
Jim Martens <github@2martens.de>
2015-01-15 11:43:08 +0100

    Added copyright information
    Signed-off-by: Jim Martens <github@2martens.de>

--- conf.py
+++ conf.py
@@ -73,7 +73,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = '2martens Web Platform'
-copyright = ''
+copyright = '2015 Jim Martens'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
